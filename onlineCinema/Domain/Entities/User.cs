1acbaca2a11fd9d18b1b85453622c273b4642f27
Folonder <74664782+Folonder@users.noreply.github.com>
2023-03-24 14:11:14 +0300

    add Film and User models

---
+++ onlineCinema/Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public static int Id { get; set; }
+        
+        public static string Username { get; set; }
+        
+        public static string Password { get; set; }
+        
+        public static string Email { get; set; }
+        
+        public static DateTime CreateTime { get; set; }
+        
+    }
+}
